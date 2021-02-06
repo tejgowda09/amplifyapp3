bab968c1fd3d2696a1beb269fc401f983439631f
thejas <thejastanzy@gmail.com>
2021-02-06 18:38:32 +0530

    auth added

--- src/App.js
+++ src/App.js
@@ -10,14 +10,9 @@ function App() {
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1> We now have auth </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        
 
-        </a>
+       
       </header>
       <AmplifySignOut />
     </div>
